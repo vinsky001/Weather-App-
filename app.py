0e452a61f3e391b35a728cb4a6827b9177a65bb4
vinsky001 <ekibet544@gmail.com>
2023-05-31 16:26:26 +0300

    A python script that gets the current weather data from Openweathermap using API-keys

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import requests
+import palse
+
+app = Flask(__name__)
+
+def fetch_weather_data(city):
+    base_url = "http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={palse.api_key}"
+    params = {
+        "q" : city,
+        "appid" : api_key,
+        "units" : "metrics"   
+    }
+    
+    try:
+        response = response.get(base_url, params=params)
+        response = response.raise_for_status()
+        data = response.Json()
+        return data
+    except response.exceptions.RequestException as e:
+        print("Error occured", e)
+        return None 
+    
+def display_weather_data(weather_data):
+    if weather_data is None:
+        print("Unable to fetch weather data")
+        return
+  
+  
+@app.route('/search', methods=['POST'])
+def search():
+    city_name = request.form["city"]
+    weather_data = fetch_weather_data(city.name)
+    display_weather_data(weather_data)
+    return render_template(app.index.html, weather_data=weather_data) 
+
+if __name__=="__main__":
+    app.run(debug=True)         
