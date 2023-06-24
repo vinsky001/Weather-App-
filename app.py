dd9b81b724f9f5637e8fb3ee139619d51df2c940
Houdini Papi <ochiengcliff.co@gmail.com>
2023-06-24 19:31:40 +0300

    Creating Python program that retrieves data from the OpenWeather API

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Import necessary modules
 from flask import Flask, render_template, request
 import requests
 from requests import Response
@@ -5,58 +6,97 @@ import os
 from dotenv import load_dotenv
 import json
 
+# Create a Flask application instance
 app = Flask(__name__)
 
 
+# Load environment variables
 def configure():
     load_dotenv()
 
-@app.route('/', methods=['GET', 'POST'])
+
+# Define route for root URL with GET and POST methods
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        city = request.form['city']
+    # Check if request method is POST
+    if request.method == "POST":
+        # Retrieve value of 'city' form field submitted by user
+        city = request.form["city"]
+
+        # Call get_weather() function to retrieve weather data
         weather_data = get_weather(city)
-        return render_template('index.html', weather_data=weather_data)
-    return render_template('index.html')
+
+        # Render 'index.html' template with weather data
+        return render_template("index.html", weather_data=weather_data)
+
+    # Render 'index.html' template without weather data
+    return render_template("index.html")
 
 
 def get_weather(city):
+    # Retrieve API key from environment variables
     api_key = os.getenv("API_KEY")
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+
+    # Construct API URL
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+
+    # Send GET request to API
     response = requests.get(url)
+
+    # Parse response JSON
     data = json.loads(response.text)
 
-    if 'weather' in data and 'temp' in data['main']:
-        temperature = round(data['main']['temp'] - 273.15, 2)
-        humidity = data['main']['humidity']
-        wind_speed = data['wind']['speed']
-    else:
-        temperature = 'N/A'
-        humidity = 'N/A'
-        wind_speed = 'N/A'
-    
-    if 'weather' in data and len(data['weather']) > 0:
-        description = data['weather'][0]['description']
-        icon = data['weather'][0]['icon']
-    else:
-        description = 'N/A'
-        icon = 'N/A'
+    # Check if weather and temperature data is available
+    if "weather" in data and "temp" in data["main"]:
+        # Calculate temperature in Celsius
+        temperature = round(data["main"]["temp"] - 273.15, 2)
 
+        # Retrieve humidity
+        humidity = data["main"]["humidity"]
+
+        # Retrieve wind speed
+        wind_speed = data["wind"]["speed"]
+    else:
+        # Set temperature to 'N/A' if data is not available
+        temperature = "N/A"
+
+        # Set humidity to 'N/A' if data is not available
+        humidity = "N/A"
+
+        # Set wind speed to 'N/A' if data is not available
+        wind_speed = "N/A"
+
+    # Check if weather description is available
+    if "weather" in data and len(data["weather"]) > 0:
+        # Retrieve weather description
+        description = data["weather"][0]["description"]
+
+        # Retrieve weather icon
+        icon = data["weather"][0]["icon"]
+    else:
+        # Set description to 'N/A' if data is not available
+        description = "N/A"
+
+        # Set icon to 'N/A' if data is not available
+        icon = "N/A"
+
+    # Create dictionary containing weather information
     weather_data = {
-        'city': city,
-        'temperature': temperature,
-        'description': description.title(),
-        'icon': icon,
-        'humidity': humidity,
-        'wind_speed': wind_speed
+        "city": city,
+        "temperature": temperature,
+        "description": description.title(),
+        "icon": icon,
+        "humidity": humidity,
+        "wind_speed": wind_speed,
     }
 
+    # Return weather data dictionary
     return weather_data
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # Run the Flask application if script is executed directly
     app.run(debug=True)
-
 
 
 ###################### ---> TRIAL <--- ######################
@@ -66,9 +106,9 @@ if __name__ == '__main__':
 #     params = {
 #         "q" : city,
 #         "appid" : api_key,
-#         "units" : "metric"   
+#         "units" : "metric"
 #     }
-    
+
 #     try:
 #         response = requests.get(base_url.format(city=city), params=params)
 #         response.raise_for_status()
@@ -76,26 +116,25 @@ if __name__ == '__main__':
 #         return data
 #     except response.exceptions.RequestException as e:
 #         print("Error occured:", e)
-#         return None 
-    
+#         return None
+
 # def display_weather_data(weather_data):
 #     if weather_data is None:
 #         print("Unable to fetch weather data")
 #     else:
 #         print(weather_data)
-    
+
 # @app.route('/')
 # def index():
-#      return render_template("index.html")    
-        
-  
-  
+#      return render_template("index.html")
+
+
 # @app.route('/search', methods=['POST', 'GET'])
 # def search():
 #     city_name = request.form['city']
 #     weather_data = fetch_weather_data(city_name)
 #     display_weather_data(weather_data)
-#     return render_template('index.html', weather_data=weather_data) 
+#     return render_template('index.html', weather_data=weather_data)
 
 # if __name__=="__main__":
 #     configure()
